5c289a380911fe0088d4a0108a8163691047b23d
andre-janssen <Andre_Janssen@gmx.de>
2026-06-09 15:16:45 +0200

    feat: complete import_eurocontrol.py with all three fact tables

--- scripts/import/import_eurocontrol.py
+++ scripts/import/import_eurocontrol.py
@@ -11,8 +11,17 @@ load_dotenv()
 DB_URL = os.getenv('DATABASE_URL')
 engine = create_engine(DB_URL)
 
+# Empty fact tables
+with engine.connect() as conn:
+    conn.execute(text("TRUNCATE TABLE fact_airport_delay RESTART IDENTITY CASCADE;"))
+    conn.execute(text("TRUNCATE TABLE fact_airport_traffic RESTART IDENTITY CASCADE;"))
+    conn.execute(text("TRUNCATE TABLE fact_enroute_delay RESTART IDENTITY CASCADE;"))
+    conn.commit()
+
 # Years to import
 YEARS_LIST = list(range(2016, 2027))
+
+
 
 # -------------- Import fact_airport_delay -------------------------------------
 
@@ -30,6 +39,7 @@ for y in YEARS_LIST:
     df_temp = pd.read_csv(path, encoding='latin-1')
     df_apt_dly = pd.concat([df_apt_dly, df_temp])
 
+
 # -- Transform --
 
 df_apt_dly["FLT_DATE"] = pd.to_datetime(df_apt_dly["FLT_DATE"], errors="coerce")
@@ -39,14 +49,11 @@ df_apt_dly = df_apt_dly.drop(columns="ATFM_VERSION")
 
 df_apt_dly.columns = [col.lower() for col in df_apt_dly.columns]
 
+
 # -- Load --
 
-# Clear the table before importing
-with engine.connect() as conn:
-    conn.execute(text("TRUNCATE TABLE fact_airport_delay RESTART IDENTITY CASCADE;"))
-    conn.commit()
-
 df_apt_dly.to_sql("fact_airport_delay", engine, if_exists='append', index=False)
+print("-- fact_airport_delay finished --")
 
 
 
@@ -66,6 +73,7 @@ for y in YEARS_LIST:
     df_temp = pd.read_csv(path, encoding='latin-1')
     df_ert_dly = pd.concat([df_ert_dly, df_temp])
 
+
 # -- Transform --
 
 df_ert_dly["FLT_DATE"] = pd.to_datetime(df_ert_dly["FLT_DATE"], errors="coerce")
@@ -73,14 +81,12 @@ df_ert_dly["FLT_DATE"] = df_ert_dly["FLT_DATE"].dt.tz_localize(None)
 
 df_ert_dly.columns = [col.lower() for col in df_ert_dly.columns]
 
+
 # -- Load --
 
-# Clear the table before importing
-with engine.connect() as conn:
-    conn.execute(text("TRUNCATE TABLE fact_enroute_delay RESTART IDENTITY CASCADE;"))
-    conn.commit()
-
 df_ert_dly.to_sql("fact_enroute_delay", engine, if_exists='append', index=False)
+print("-- fact_enroute_delay finished --")
+
 
 
 # -------------- Import fact_airport_traffic -----------------------------------
@@ -99,6 +105,7 @@ for y in YEARS_LIST:
     df_temp = pd.read_csv(path, encoding='latin-1')
     df_apt_tfc = pd.concat([df_apt_tfc, df_temp])
 
+
 # -- Transform --
 
 df_apt_tfc["FLT_DATE"] = pd.to_datetime(df_apt_tfc["FLT_DATE"], errors="coerce")
@@ -106,13 +113,16 @@ df_apt_tfc["FLT_DATE"] = df_apt_tfc["FLT_DATE"].dt.tz_localize(None)
 
 df_apt_tfc = df_apt_tfc.drop(columns=["FLT_DEP_IFR_2", "FLT_ARR_IFR_2", "FLT_TOT_IFR_2"])
 
+df_apt_tfc = df_apt_tfc.drop_duplicates(subset=["APT_ICAO", "FLT_DATE"], keep="first")
+
 df_apt_tfc.columns = [col.lower() for col in df_apt_tfc.columns]
+
 
 # -- Load --
 
-# Clear the table before importing
-with engine.connect() as conn:
-    conn.execute(text("TRUNCATE TABLE fact_airport_traffic RESTART IDENTITY CASCADE;"))
-    conn.commit()
-
 df_apt_tfc.to_sql("fact_airport_traffic", engine, if_exists='append', index=False)
+print("-- fact_airport_traffic finished --")
+
+
+
+print("-- import_eurocontrol.py finished --")
